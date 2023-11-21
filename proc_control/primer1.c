b7da424a484766a91f0b62076910f403fedf0410
CharlesCHL <87629980+CharlesCHL@users.noreply.github.com>
2023-11-22 01:13:32 +0800

    2023/11/22-1th

---
+++ proc_control/primer1.c
@@ -0,0 +1,51 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define LEFT    30000000
+#define RIGHT   30000200
+
+int main()
+{
+    int i;
+    int j;
+    int mark;
+    pid_t pid;
+
+    for (i = LEFT; i <= RIGHT; i++)
+    {
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("fork()");
+            exit(1);
+        }
+        if (pid == 0)
+        {
+            mark = 1;
+            for (j = 2; j < i/2; j++)
+            {
+                if (i % j == 0)
+                {
+                    mark = 0;
+                    break;
+                }
+            }
+            if (mark)
+                printf("%d is a primer\n", i);
+//            sleep(100);
+            exit(1);
+        }
+    }
+
+    sleep(1000);
+
+    exit(0);
+
+}
+
+
+
+
